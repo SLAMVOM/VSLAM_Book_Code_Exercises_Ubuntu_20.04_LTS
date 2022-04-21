ac84936c1012bdc01766499ad84f665b8bba3293
SLAMVOM <MTslamvo@gmail.com>
2022-04-21 16:26:25 -0400

    Removed build folders

--- ch13/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mt/Miller/VSLAM/project/ch13/test
-# Build directory: /home/mt/Miller/VSLAM/project/ch13/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_triangulation "test_triangulation")
-set_tests_properties(test_triangulation PROPERTIES  _BACKTRACE_TRIPLES "/home/mt/Miller/VSLAM/project/ch13/test/CMakeLists.txt;6;ADD_TEST;/home/mt/Miller/VSLAM/project/ch13/test/CMakeLists.txt;0;")
